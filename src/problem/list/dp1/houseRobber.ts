4055a2e482c7b17a9bcc21b02b95d93c89d54fea
Jaroslaw Weber <jaroslawweber@gmail.com>
2024-05-11 17:24:53 +0900

    fix hosue robber

--- src/problem/list/dp1/houseRobber.ts
+++ src/problem/list/dp1/houseRobber.ts
@@ -1,9 +1,11 @@
+import { sum } from "lodash";
 import { Problem, ProblemState, Variable } from "../../types";
-import { asArray, asSimpleValue } from "../../utils";
+import { asArray, asSimpleValue, asValueGroup } from "../../utils";
 
 function rob(houses: HouseRobberInput): ProblemState[] {
   const s: ProblemState[] = [];
   const { nums } = houses;
+  const groupOptions = { min: 0, max: sum(houses.nums) };
   const n = nums.length;
   const dp: number[] = new Array(n + 1).fill(0);
   dp[0] = 0;
@@ -19,17 +21,22 @@ function rob(houses: HouseRobberInput): ProblemState[] {
   }); //#2
 
   for (let i = 2; i <= n; i++) {
-    dp[i] = Math.max(dp[i - 1], dp[i - 2] + nums[i - 1]);
+    const skipCurrent = dp[i - 1];
+    const twoHousesBefore = dp[i - 2];
+    const currentHouse = nums[i - 1];
+    const includeCurrent = twoHousesBefore + currentHouse;
+    dp[i] = Math.max(skipCurrent, includeCurrent);
     s.push({
       variables: [
         asArray("nums", nums,  i - 1),
         asArray("dp", dp, i, i - 1, i - 2),
-        ...asSimpleValue({
-          i,
-          "dp[i-1]": dp[i - 1],
-          "nums[i-1]": nums[i - 1],
-          "dp[i-2]": dp[i - 2],
-        }),
+        asValueGroup("max profit",{
+          skipCurrent,
+          includeCurrent,
+          twoHousesBefore,
+          currentHouse,
+        }, groupOptions),
+        asValueGroup("loop", { i }, groupOptions),
       ],
       breakpoint: 3,
     }); //#3
@@ -59,7 +66,11 @@ const code = `function rob(nums) {
   dp[1] = nums[0]; 
   //#2
   for (let i = 2; i <= n; i++) {
-    dp[i] = Math.max(dp[i - 1], dp[i - 2] + nums[i - 1]); 
+    const skipCurrent = dp[i - 1];
+    const currentHouse = nums[i - 1];
+    const twoHousesBefore = dp[i - 2];
+    const includeCurrent = twoHousesBefore + currentHouse;
+    dp[i] = Math.max(skipCurrent, includeCurrent);  
     //#3
   }
   const result = dp[n]; 
