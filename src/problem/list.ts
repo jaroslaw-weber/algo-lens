3375e30973589b26c6a452fff2d40bd5d14cad77
Jaroslaw Weber <jaroslawweber@gmail.com>
2024-05-15 20:48:32 +0900

    reafactor two sum

--- src/problem/list.ts
+++ src/problem/list.ts
@@ -8,7 +8,7 @@ import { longestIncreasingSubsequenceProblem } from "./list/dp1/longestIncreasin
 import { minPathSumProblem } from "./list/dp2/minimumPathSum";
 import { knapsackProblem } from "./list/dp2/knapsackProblem";
 import { uniquePathsProblem } from "./list/dp2/uniquePaths";
-import { twoSumProblem } from "./list/twoSum";
+import { twoSumProblem } from "./list/two-sum";
 import { maxAreaProblem } from "./list/container-with-most-water";
 import { countBitsProblem } from "./list/counting-bits";
 import { hammingWeightProblem } from "./list/number-of-1-bits";
