ad3f9a9d31f4b2caf12feed23a0b39cc4a12c36d
Jaroslaw Weber <jaroslawweber@gmail.com>
2024-05-21 23:04:56 +0900

    fix course schedule

--- src/problem/list/course-schedule.ts
+++ src/problem/list/course-schedule.ts
@@ -1,4 +1,4 @@
-import { Problem, ProblemState, ThemeColor } from "../types";
+import { Problem, ProblemState, ThemeColor, Variable } from "../types";
 import {
   asArray,
   as2dArray,
@@ -20,37 +20,85 @@ export function courseSchedule(p: CourseScheduleInput): ProblemState[] {
   const graph: number[][] = new Array(numCourses).fill(0).map(() => []);
   const visited: number[] = new Array(numCourses).fill(0);
 
+  // Helper function to create and log each step's computational state
+  function log(
+    point: number,
+    p?: {
+      course?: number;
+      prerequisiteCourse?: number;
+      visitedIndex?: number;
+      graphIndex?: number;
+    }
+  ) {
+    const v: Variable[] = [];
+    const step: ProblemState = {
+      variables: v,
+      breakpoint: point,
+    };
+    v.push(as2dArray("graph", graph, []));
+    v.push(asArray("visited", visited));
+    v.push(...asSimpleValue({ numCourses }));
+    v.push(as2dArray("prerequisites", prerequisites, []));
+    if (p) {
+      const { course, prerequisiteCourse } = p;
+      if (course) {
+        v.push(...asSimpleValue({ course }));
+      }
+      if (prerequisiteCourse) {
+        v.push(...asSimpleValue({ prerequisiteCourse }));
+      }
+    }
+    steps.push(step);
+  }
+
+  log(1);
   for (let i = 0; i < numCourses; i++) {
     graph[i] = [];
   }
+  log(2);
 
   for (let i = 0; i < prerequisites.length; i++) {
-    graph[prerequisites[i][0]].push(prerequisites[i][1]);
+    const prerequisite = prerequisites[i];
+    const course = prerequisite[0];
+    const prerequisiteCourse = prerequisite[1];
+    log(3, {
+      course,
+      prerequisiteCourse,
+    });
+    graph[course].push(prerequisiteCourse);
   }
 
   function dfs(i: number): boolean {
     if (visited[i] === -1) {
+      log(4, { visitedIndex: i });
       return false;
     }
     if (visited[i] === 1) {
+      log(5, { visitedIndex: i });
       return true;
     }
     visited[i] = -1;
+    log(6, { visitedIndex: i });
     for (const j of graph[i]) {
+      log(7, { graphIndex: j });
       if (!dfs(j)) {
+        log(8, { graphIndex: j });
         return false;
       }
     }
+    log(9, { visitedIndex: i });
     visited[i] = 1;
     return true;
   }
 
   for (let i = 0; i < numCourses; i++) {
     if (!dfs(i)) {
+      log(10);
       return steps;
     }
   }
 
+  log(11);
   return steps;
 }
 
@@ -91,13 +139,19 @@ const code = `function canFinish(numCourses: number, prerequisites: number[][]):
 const title = "Course Schedule";
 const getInput = () => ({
   numCourses: 4,
-  prerequisites: [[1, 0], [2, 0], [3, 1], [3, 2]],
+  prerequisites: [
+    [1, 0],
+    [2, 0],
+    [3, 1],
+    [3, 2],
+  ],
 });
 
-export const courseScheduleProblem: Problem<CourseScheduleInput, ProblemState> = {
-  title,
-  code,
-  getInput,
-  func: courseSchedule,
-  id: "course-schedule",
-};
+export const courseScheduleProblem: Problem<CourseScheduleInput, ProblemState> =
+  {
+    title,
+    code,
+    getInput,
+    func: courseSchedule,
+    id: "course-schedule",
+  };
