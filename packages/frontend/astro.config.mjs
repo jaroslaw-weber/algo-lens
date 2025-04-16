4391e9f3e1012f8f44691407bcc8b0678b2ded9e
Jaroslaw Weber <jaroslawweber@gmail.com>
2025-04-16 22:32:06 +0900

    add tailwind

--- packages/frontend/astro.config.mjs
+++ packages/frontend/astro.config.mjs
@@ -2,16 +2,24 @@
 import { defineConfig, envField } from 'astro/config';
 import react from '@astrojs/react';
 
+import tailwindcss from "@tailwindcss/vite";
+
 // https://astro.build/config
 export default defineConfig({
-	env: {
-		schema: {
-			BACKEND_URL: envField.string({ context: "client", access: "public", optional: true }),
-		}
+  env: {
+      schema: {
+          BACKEND_URL: envField.string({ context: "client", access: "public", optional: true }),
+      }
 	},
-	server:{
+
+  server:{
 
 	},
-	//
-	integrations: [react()],
+
+  //
+  integrations: [react()],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
