c53de38a6bfbd9c261c6a9c049a98ebbe2d33364
Jaroslaw Weber <jaroslaw.weber@adriel.com>
2025-05-03 20:23:52 +0900

    fix tests for besttimetobuy and sell stocks

--- packages/backend/src/problem/free/bestTimeToBuyAndSellStocks/testcase.ts
+++ packages/backend/src/problem/free/bestTimeToBuyAndSellStocks/testcase.ts
@@ -1,11 +1,22 @@
 import { ProblemState, TestCase } from "algo-lens-core";
 import { MaxProfitInput } from "./types"; // Assuming MaxProfitInput is defined in types.ts
 
-export const testcases: TestCase<MaxProfitInput, ProblemState>[] = [
-  { input: { prices: [2, 1, 2, 0, 1, 2] }, expected: { variables: [{ label: "maxProfit", type: "number", value: 2 }] } },
-  { input: { prices: [7, 1, 5, 3, 6, 4] }, expected: { variables: [{ label: "maxProfit", type: "number", value: 5 }] } },
-  { input: { prices: [7, 6, 4, 3, 1] }, expected: { variables: [{ label: "maxProfit", type: "number", value: 0 }] } },
-  { input: { prices: [1, 2, 3, 4, 5] }, expected: { variables: [{ label: "maxProfit", type: "number", value: 4 }] } },
-  { input: { prices: [1] }, expected: { variables: [{ label: "maxProfit", type: "number", value: 0 }] } },
-  { input: { prices: [] }, expected: { variables: [{ label: "maxProfit", type: "number", value: 0 }] } },
+export const testcases: TestCase<MaxProfitInput, number>[] = [
+  { input: [2, 1, 2, 0, 1, 2], expected: 2 },
+  {
+    input: [7, 1, 5, 3, 6, 4],
+    expected: 5,
+  },
+  {
+    input: [7, 6, 4, 3, 1],
+    expected: 0,
+  },
+  {
+    input: [1, 2, 3, 4, 5],
+    expected: 4,
+  },
+  {
+    input: [1],
+    expected: 0,
+  },
 ];
