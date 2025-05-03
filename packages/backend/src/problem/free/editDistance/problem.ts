e0ecb534ded02cb393e6059c2ec6f53f6f21d9b3
Jaroslaw Weber <jaroslaw.weber@adriel.com>
2025-05-03 19:34:20 +0900

    fixes

--- packages/backend/src/problem/free/editDistance/problem.ts
+++ packages/backend/src/problem/free/editDistance/problem.ts
@@ -2,21 +2,21 @@ import { Problem, ProblemState } from "algo-lens-core";
 import { generateSteps } from "./steps"; // Corrected import name
 import { EditDistanceInput } from "./types";
 import { code } from "./code/typescript";
-import { variableMetadata } from "./variables";
 import { groups } from "./groups";
+import { variables } from "./variables";
 
 const title = "Edit Distance";
 const getInput = (): EditDistanceInput => ({ s1: "kitten", s2: "sitting" });
 
 export const problem: Problem<EditDistanceInput, ProblemState> = {
   title,
-  emoji: '✏️',
+  emoji: "✏️",
   id: "edit-distance",
   tags: ["dynamic programming", "string"],
   code,
   func: generateSteps, // Corrected function assignment
   metadata: {
-    variableMetadata,
+    variables,
     groups,
-  }
+  },
 };
