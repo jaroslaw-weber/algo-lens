148ee83708dc42ce29682983cbb4b914c47b8c2e
Jaroslaw Weber <jaroslaw.weber@adriel.com>
2025-05-03 20:06:57 +0900

    fix testing to be more flexible

--- packages/backend/src/problem/core/test.ts
+++ packages/backend/src/problem/core/test.ts
@@ -1,15 +1,20 @@
-import { Problem } from "algo-lens-core";
+import { Problem, ProblemState } from "algo-lens-core";
 import { cloneDeep, last } from "lodash";
 import { describe, it, expect } from "bun:test";
 
-export function runTests(problem: Problem<any, any>) {
-	for (const testCase of problem.testcases) {
-	  const input = cloneDeep(testCase.input);
-	  const expected = cloneDeep(testCase.expected);
-	  const states = problem.func(input);
-	  const state = last(states)
-  
-	  expect(state).toEqual(expected);
-	}
-	
+export function runTests(problem: Problem<any, ProblemState>) {
+  for (const testcase of problem.testcases) {
+    const input = cloneDeep(testcase.input);
+    const expected = cloneDeep(testcase.expected);
+    const states = problem.func(input);
+    const state = last(states);
+    const variables = state!.variables;
+    const result = variables.find((x) => x.label === "result");
+    if (!result) {
+      throw new Error("No result found in last state");
+    }
+    //@ts-expect-error
+    const value = result.value;
+    expect(value).toEqual(expected);
   }
+}
