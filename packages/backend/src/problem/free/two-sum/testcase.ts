8cfa7f5d90c0f92762cfe52d91df50d0da874db3
Jaroslaw Weber <jaroslaw.weber@adriel.com>
2025-05-06 20:43:13 +0900

    fix errors

--- packages/backend/src/problem/free/two-sum/testcase.ts
+++ packages/backend/src/problem/free/two-sum/testcase.ts
@@ -1,10 +1,9 @@
 import { ProblemState, TestCase } from "algo-lens-core";
 import { TwoSumInput } from "./types";
 
-export const testcases = [
+export const testcases: TestCase<TwoSumInput, ProblemState>[] = [
   { input: [[2, 7, 11, 15], 9], expected: [0, 1] },
   { input: [[3, 2, 4], 6], expected: [1, 2] },
-  { input: [[3, 3], 6], expected: [0, 1] ,
-    isDefault: true},
-  { input: [[-1, 0], -1], expected: [0, 1] }
+  { input: [[3, 3], 6], expected: [0, 1], isDefault: true },
+  { input: [[-1, 0], -1], expected: [0, 1] },
 ];
