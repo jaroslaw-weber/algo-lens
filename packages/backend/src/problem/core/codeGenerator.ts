c40a080234255728f0de49cf48df183adbcea5b5
Jaroslaw Weber <jaroslaw.weber@adriel.com>
2025-05-10 23:26:20 +0900

    fix codegen

--- packages/backend/src/problem/core/codeGenerator.ts
+++ packages/backend/src/problem/core/codeGenerator.ts
@@ -14,10 +14,14 @@ export function generateCodeFromSteps(
   let result = params.stepsFileContent;
   //start with 'l.TEXT' and end with ; (include multiline)
   result = result.replace(/l\.[\s\S]*?;/g, "");
-  //todo: remove extra empty lines (remove if 2 or more empty lines)
-  //todo: remove step logger line
-  //todo: remove imports
+  // Remove extra empty lines (remove if 2 or more empty lines)
+  result = result.replace(/(\n\s*){2,}/g, "\n");
+  // Remove step logger line
+  result = result.replace(/stepLogger\..*?;/g, "");
+  // Remove imports
+  result = result.replace(/^import[\s\S]*?;?\n/gm, "");
 
+  result = result.replace(/^\s*\/\/.*$\n/gm, "");
   const content = result;
   return { content };
 }
