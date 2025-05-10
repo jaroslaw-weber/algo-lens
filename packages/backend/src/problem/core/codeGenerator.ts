260456f0bd4a8ca1006d25c52c464aafb4ad547c
Jaroslaw Weber <jaroslaw.weber@adriel.com>
2025-05-10 20:54:19 +0900

    fix code gen to handle multi lines

--- packages/backend/src/problem/core/codeGenerator.ts
+++ packages/backend/src/problem/core/codeGenerator.ts
@@ -22,8 +22,8 @@ export function generateCodeFromSteps(
 ): GeneratedCodeOutput {
   const lines = params.stepsFileContent.split("\n");
 
-  const result = lines.filter(includeLine);
-  const content = result.join("\n");
+  const result = params.stepsFileContent.replace(/l\..*?[=:].*?(\n|$)/g, ""); // Remove any string pattern starting with l. and ending with : or =, including multiline
 
+  const content = result;
   return { content };
 }
