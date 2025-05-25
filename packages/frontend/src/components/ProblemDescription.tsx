11eaa24cd37ea1ae86388a539f815ae2bbe43972
Jaroslaw Weber <jaroslaw.weber@adriel.com>
2025-05-25 22:46:11 +0900

    improve description

--- packages/frontend/src/components/ProblemDescription.tsx
+++ packages/frontend/src/components/ProblemDescription.tsx
@@ -1,21 +1,38 @@
-import React from 'react';
+import React, { useState } from "react";
 
 interface ProblemDescriptionProps {
   description: string | undefined;
 }
 
-const StateDescription: React.FC<ProblemDescriptionProps> = ({ description }) => {
+const ProblemDescription: React.FC<ProblemDescriptionProps> = ({
+  description,
+}) => {
+  const [isHidden, setIsHidden] = useState(false);
+
   if (!description) {
     return null;
   }
 
   return (
-    <div className="mb-4 mt-4 p-4 border border-dashed border-gray-400 rounded-lg bg-gray-50 w-full text-center min-h-24">
-      <p className="text-sm font-semibold text-gray-700">
-        {description}
-      </p>
+    <div className="mb-4 mt-4 w-full">
+      {!isHidden && (
+        <div className="relative pt-8 p-4 border border-dashed border-gray-400 rounded-lg bg-gray-50 w-full text-center min-h-24 mt-2">
+          <div className="absolute -top-3 left-1/2 transform -translate-x-1/2 bg-gray-50 px-2 text-gray-500 text-sm font-semibold">
+            💡 what is going on
+          </div>
+          <button
+            onClick={() => setIsHidden(true)}
+            className="absolute top-2 right-4 text-gray-700 hover:text-gray-900 focus:outline-none"
+          >
+            &#x2715;
+          </button>
+          <p className="text-sm font-semibold text-gray-700 px-4">
+            {description}
+          </p>
+        </div>
+      )}
     </div>
   );
 };
 
-export default StateDescription;
+export default ProblemDescription;
