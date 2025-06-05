db894fc79fd753bb9b496488f2d3e45c46547f80
Jaroslaw Weber <jaroslaw.weber@adriel.com>
2025-06-05 21:36:40 +0900

    fix

--- packages/backend/src/problem/free/reverse-list/testcase.ts
+++ packages/backend/src/problem/free/reverse-list/testcase.ts
@@ -5,11 +5,12 @@ import { ReverseListInput } from "./types";
 export const testcases = [
   {
     input: { head: new ListNode(1) },
-    expected: (() => {
-      const node = new ListNode(1);
-      node.id = "1";
-      return node;
-    })(),
+    expected: [
+      {
+        next: null,
+        value: 1,
+      },
+    ],
     name: "Single Element List",
     description: "Single element list",
   },
@@ -24,19 +25,12 @@ export const testcases = [
     },
     // Expected: 5 -> 4 -> 3 -> 2 -> 1
     // Manually create 5 -> 4 -> 3 -> 2 -> 1
-    expected: (() => {
-      const node1 = new ListNode(1);
-      node1.id = "5";
-      const node2 = new ListNode(2, node1);
-      node2.id = "4";
-      const node3 = new ListNode(3, node2);
-      node3.id = "3";
-      const node4 = new ListNode(4, node3);
-      node4.id = "2";
-      const node5 = new ListNode(5, node4);
-      node5.id = "1";
-      return node5;
-    })(),
+    expected: [
+      {
+        next: null,
+        value: 1,
+      },
+    ],
     name: "Default 5 Elements",
     description: "List with 5 elements", // Updated description
     isDefault: true,
@@ -46,13 +40,12 @@ export const testcases = [
     input: { head: new ListNode(5, new ListNode(8)) },
     // Expected: 8 -> 5
     // Manually create 8 -> 5
-    expected: (() => {
-      const node1 = new ListNode(5);
-      node1.id = "2";
-      const node2 = new ListNode(8, node1);
-      node2.id = "1";
-      return node2;
-    })(),
+    expected: [
+      {
+        next: null,
+        value: 1,
+      },
+    ],
     name: "Two Elements",
     description: "List with multiple elements (5->8)",
   },
@@ -61,15 +54,12 @@ export const testcases = [
     input: { head: new ListNode(1, new ListNode(2, new ListNode(3))) },
     // Expected: 3 -> 2 -> 1
     // Manually create 3 -> 2 -> 1
-    expected: (() => {
-      const node1 = new ListNode(1);
-      node1.id = "3";
-      const node2 = new ListNode(2, node1);
-      node2.id = "2";
-      const node3 = new ListNode(3, node2);
-      node3.id = "1";
-      return node3;
-    })(),
+    expected: [
+      {
+        next: null,
+        value: 1,
+      },
+    ],
     name: "Three Elements",
     description: "List with three elements (1->2->3)",
   },
