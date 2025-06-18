cb89f2e38a921e16c4299c58a38c57078a310d84
Jaroslaw Weber <jaroslawweber@gmail.com>
2025-06-18 21:50:09 +0900

    fixes

--- packages/backend/src/problem/free/spiral-matrix/steps.ts
+++ packages/backend/src/problem/free/spiral-matrix/steps.ts
@@ -1,7 +1,6 @@
 import { ProblemState } from "algo-lens-core";
 import { StepLoggerV2 } from "algo-lens-core/src/StepLoggerV2";
 import { ProblemState, Pointer2D } from "algo-lens-core";
-import { StepLoggerV2 } from "../../core/StepLoggerV2";
 import { SpiralMatrixInput, SpiralMatrixOutput } from "./types";
 
 // HIDE_START
