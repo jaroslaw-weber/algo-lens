29f16ca808f1b5598f588935d6364e4b0ecf64f3
Jaroslaw Weber <jaroslawweber@gmail.com>
2025-06-18 23:12:22 +0900

    improve loading problems from new package

---
+++ packages/backend/src/problem/core/ProblemPaths.ts
@@ -0,0 +1,63 @@
+import * as fs from "fs/promises";
+import * as path from "path";
+
+interface ProblemFolderInfo {
+  problemId: string;
+  path: string;
+}
+
+export class ProblemPaths {
+  initialized = false;
+
+  private paths = new Map<string, ProblemFolderInfo>();
+
+  async getProblemFolderInfo(problemId: string) {
+    await this.init();
+    return this.paths.get(problemId);
+  }
+
+  private async loadProblemsFromDirectory(
+    dir: string
+  ): Promise<ProblemFolderInfo[]> {
+    const problemFolders = await fs.readdir(dir, { withFileTypes: true });
+    return problemFolders
+      .filter((dirent) => dirent.isDirectory())
+      .map((dirent) => ({
+        problemId: dirent.name,
+        path: path.join(dir, dirent.name),
+      }));
+  }
+
+  async getList() {
+    await this.init();
+    return Array.from(this.paths.values());
+  }
+
+  async init() {
+    if (this.initialized) {
+      return;
+    }
+    {
+      const dir = path.join(__dirname, "../free")
+      console.log("dir1", dir)
+      const info = await this.loadProblemsFromDirectory(dir);
+      this.add(info)
+    }
+    {
+      const dir =  path.join(__dirname, "../../../../problem-free");
+      console.log("dir2", dir)
+      const info = await this.loadProblemsFromDirectory(dir);
+      this.add(info)
+    }
+
+    this.initialized = true;
+  }
+
+  add(arr: ProblemFolderInfo[]) {
+    for (const info of arr) {
+      this.paths.set(info.problemId, info);
+    }
+  }
+}
+
+export const problemPaths = new ProblemPaths();
